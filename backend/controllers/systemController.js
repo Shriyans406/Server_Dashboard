8f48cc2171bdfe697c1613d1652ea9c8c4823528
Shriyans406 <shriyans.s.sahoo@gmail.com>
2026-03-12 09:07:16 +0000

    phase 3

--- backend/controllers/systemController.js
+++ backend/controllers/systemController.js
@@ -4,8 +4,66 @@ const runScript = require("../utils/scriptRunner");
 exports.getCPU = async (req, res) => {
   try {
     const script = path.join(__dirname, "../scripts/system/cpu_usage.sh");
-    const result = await runScript(script);
-    res.json({ result });
+    const output = await runScript(script);
+
+    const value = output.split("=")[1];
+
+    res.json({
+      metric: "cpu",
+      value: Number(value)
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: error.toString() });
+  }
+};
+
+exports.getMemory = async (req, res) => {
+  try {
+    const script = path.join(__dirname, "../scripts/system/memory_usage.sh");
+    const output = await runScript(script);
+
+    const value = output.split("=")[1];
+
+    res.json({
+      metric: "memory",
+      value: Number(value)
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: error.toString() });
+  }
+};
+
+exports.getDisk = async (req, res) => {
+  try {
+    const script = path.join(__dirname, "../scripts/system/disk_usage.sh");
+    const output = await runScript(script);
+
+    const value = output.split("=")[1];
+
+    res.json({
+      metric: "disk",
+      value: Number(value)
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: error.toString() });
+  }
+};
+
+exports.getUptime = async (req, res) => {
+  try {
+    const script = path.join(__dirname, "../scripts/system/uptime.sh");
+    const output = await runScript(script);
+
+    const value = output.split("=")[1];
+
+    res.json({
+      metric: "uptime",
+      value: value
+    });
+
   } catch (error) {
     res.status(500).json({ error: error.toString() });
   }
--- backend/routes/systemRoutes.js
+++ backend/routes/systemRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const systemController = require("../controllers/systemController");
 
 router.get("/cpu", systemController.getCPU);
+router.get("/memory", systemController.getMemory);
+router.get("/disk", systemController.getDisk);
+router.get("/uptime", systemController.getUptime);
 
 module.exports = router;
